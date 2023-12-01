c12b9aede2fe6d739a4dace5832645c27b762aa4
刘煜松 <yusong.liu@1hai.cn>
2023-12-01 18:58:36 +0800

    init controller

--- SimpleWebApi/Controllers/WeatherForecastController.cs
+++ SimpleWebApi/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace SimpleWebApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]/[action]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
