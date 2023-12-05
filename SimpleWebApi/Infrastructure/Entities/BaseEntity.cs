5f3257dd87809202cc7adb159b638a769140a033
刘煜松 <yusong.liu@1hai.cn>
2023-12-05 09:51:11 +0800

    fix name

--- SimpleWebApi/Infrastructure/Entities/BaseEntity.cs
+++ SimpleWebApi/Infrastructure/Entities/BaseEntity.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace SimpleWebApi.Infrastructure.Entitys;
+namespace SimpleWebApi.Infrastructure.Entities;
 
 public class BaseEntity
 {
