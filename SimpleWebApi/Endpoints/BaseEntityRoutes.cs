f1514bd047eb2aeccccaa9dda7f19817acba7c1f
刘煜松 <yusong.liu@1hai.cn>
2023-12-13 11:17:05 +0800

    init mini api

---
+++ SimpleWebApi/Endpoints/BaseEntityRoutes.cs
@@ -0,0 +1,14 @@
+﻿using Carter;
+
+namespace SimpleWebApi.Endpoints;
+/// <summary>
+/// fast mini api
+/// it can be replace for controller
+/// </summary>
+public class BaseEntityRoutes:ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("test", () => Task.FromResult(Results.Ok()));
+    }
+}
