d5f81d267a0438cf36f7c7ac85559d21c69db66a
刘煜松 <yusong.liu@1hai.cn>
2023-12-14 11:04:47 +0800

    Add DI Extension Methods

---
+++ SimpleWebApi/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleWebApi.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        //services.AddScoped<ITest,Test>();
+        return services;
+    }
+    
+}
