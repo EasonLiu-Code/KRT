b9297e1cb4f6097ea7b992cb588044625747037b
刘煜松 <yusong.liu@1hai.cn>
2023-12-19 16:17:27 +0800

    Add point

--- SimpleWebApi/Endpoints/BaseEntityRoutes.cs
+++ SimpleWebApi/Endpoints/BaseEntityRoutes.cs
@@ -1,4 +1,5 @@
 ﻿using Carter;
+using Newtonsoft.Json;
 
 namespace SimpleWebApi.Endpoints;
 /// <summary>
@@ -10,5 +11,6 @@ public class BaseEntityRoutes:ICarterModule
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapGet("Test", () => Task.FromResult(Results.Ok("okay")));
+        app.MapGet("Test2", () => Task.FromResult(Results.Ok(JsonConvert.SerializeObject(typeof(WeatherForecast)))));
     }
 }
