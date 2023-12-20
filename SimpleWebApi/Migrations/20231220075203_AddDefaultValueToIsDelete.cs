8e364824b015f6fe170be63dec6618a4f2652845
刘煜松 <yusong.liu@1hai.cn>
2023-12-20 15:55:42 +0800

    数据更新

---
+++ SimpleWebApi/Migrations/20231220075203_AddDefaultValueToIsDelete.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultValueToIsDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "TestDb",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false,
+                comment: "是否删除",
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)",
+                oldComment: "是否删除");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "TestDb",
+                type: "tinyint(1)",
+                nullable: false,
+                comment: "是否删除",
+                oldClrType: typeof(bool),
+                oldType: "tinyint(1)",
+                oldDefaultValue: false,
+                oldComment: "是否删除");
+        }
+    }
+}
